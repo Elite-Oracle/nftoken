34ce758d5e11d01f5d6ebdbb894406df46457556
donflor <donflor@gmail.com>
2021-09-30 15:54:58 -0700

    Refactoring

--- src/cli/cardano.js
+++ src/cli/cardano.js
@@ -1,29 +1,29 @@
-// const CardanocliJs = require("cardanocli-js");
-// const os = require("os");
-// const path = require("path");
+const CardanocliJs = require("cardanocli-js");
+const os = require("os");
+const path = require("path");
 
-// const dir = path.join(os.homedir(), "projects/cardano-minter");
-// const shelleyPath = path.join(
-//     os.homedir(),
-//     "mainnet-relay",
-//     "mainnet-shelley-genesis.json"
-// );
+const dir = path.join(os.homedir(), "");
+const shelleyPath = path.join(
+    os.homedir(),
+    "nftoken",
+    "testnet-shelley-genesis.json"
+);
 
-// const cardanocliJs = new CardanocliJs({
-//     network: `mainnet`,
-//     dir,
-//     shelleyGenesisPath: shelleyPath,
-// });
-
-// module.exports = cardanocliJs
-
-const Cardano = require("cardanocli-js")
-
-const cardano = new Cardano({
+const cardanocliJs = new CardanocliJs({
     network: "testnet-magic 1097911063",
-    shelleyGenesisPath: "testnet-shelley-genesis.json"
-    // dir: __dirname + "/.../",
-    // shelleyGenesisPath: __dirname + "/../testnet-shelley-genesis.json"
-})
+    dir,
+    shelleyGenesisPath: shelleyPath,
+});
 
-module.exports = cardano
+module.exports = cardanocliJs
+
+// const Cardano = require("cardanocli-js")
+
+// const cardano = new Cardano({
+//     network: "testnet-magic 1097911063",
+//     shelleyGenesisPath: "testnet-shelley-genesis.json"
+//     dir: __dirname + "/.../",
+//     shelleyGenesisPath: __dirname + "/../testnet-shelley-genesis.json"
+// })
+
+// module.exports = cardano
