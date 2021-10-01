3eb584b04b1bbf63a3ff66ec2a91b88bb02f266b
donflor <donflor@gmail.com>
2021-09-30 17:20:17 -0700

    Test Param Fix

--- src/cli/mint-asset.js
+++ src/cli/mint-asset.js
@@ -49,11 +49,11 @@ const tx = {
             value: { ...wallet.balance().value, [ASSET_ID]: 1 }
         }
     ],
-    // mint: {
-    //     actions: [{ type: "mint", quantity: 1, asset: ASSET_ID }],
-    //     script: [mintScript]
-    // },
-    mint: [{ action: "mint", value: 1, token: ASSET_ID }],
+    mint: {
+        action: [{ type: "mint", quantity: 1, asset: ASSET_ID }],
+        script: [mintScript]
+    },
+    // mint: [{ action: "mint", value: 1, token: ASSET_ID }],
     metadata,
     witnessCount: 2
 }
