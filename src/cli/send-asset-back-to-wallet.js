e3b6c792ecbce84b0d25b7fe2dca9979daee83df
donflor <donflor@gmail.com>
2021-10-01 10:04:45 -0700

    Revised NFT to Wallet

--- src/cli/send-asset-back-to-wallet.js
+++ src/cli/send-asset-back-to-wallet.js
@@ -1,17 +1,13 @@
 const cardano = require("./cardano")
 
-// 1. get the wallet
-
 const sender = cardano.wallet("ISADA")
-
-// 2. define the transaction
 
 console.log(
     "Balance of Sender wallet: " +
     cardano.toAda(sender.balance().value.lovelace) + " ADA"
 )
 
-const receiver = "addr1qym6pxg9q4ussr96c9e6xjdf2ajjdmwyjknwculadjya488pqap23lgmrz38glvuz8qlzdxyarygwgu3knznwhnrq92q0t2dv0"
+const receiver = "addr_test1qqnv29mfm0c05xrgcn2p8p2g44a7rm5rfqyw9q0fx2jk2vjakdcdu7zdu3mah0c43dx94dmjvgr9e9qsxwm7faw494tqka2y75"
 
 const txInfo = {
     txIn: cardano.queryUtxo(sender.paymentAddr),
@@ -26,17 +22,13 @@ const txInfo = {
             address: receiver,
             value: {
                 lovelace: cardano.toLovelace(1.5),
-                "ad9c09fa0a62ee42fb9555ef7d7d58e782fa74687a23b62caf3a8025.BerrySpaceGreen": 1
+                "86091988254b1261a5b6f2b5938d23108cdd84439cc94cb0547338f5.ISADANFT": 1
             }
         }
     ]
 }
 
-// 3. build the transaction
-
 const raw = cardano.transactionBuildRaw(txInfo)
-
-// 4. calculate the fee
 
 const fee = cardano.transactionCalculateMinFee({
     ...txInfo,
@@ -44,22 +36,14 @@ const fee = cardano.transactionCalculateMinFee({
     witnessCount: 1
 })
 
-// 5. pay the fee by subtracting it from the sender utxo
-
 txInfo.txOut[0].value.lovelace -= fee
 
-// 6. build the final transaction
-
 const tx = cardano.transactionBuildRaw({ ...txInfo, fee })
-
-// 7. sign the transaction
 
 const txSigned = cardano.transactionSign({
     txBody: tx,
     signingKeys: [sender.payment.skey]
 })
-
-// 8. submit the transaction
 
 const txHash = cardano.transactionSubmit(txSigned)
 
