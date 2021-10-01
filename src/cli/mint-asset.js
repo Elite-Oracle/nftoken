14e09c59eb71892e7ed1c545e043ab0dd670d1c6
donflor <donflor@gmail.com>
2021-09-30 17:28:51 -0700

    Updated Mint JS

--- src/cli/mint-asset.js
+++ src/cli/mint-asset.js
@@ -49,11 +49,11 @@ const tx = {
             value: { ...wallet.balance().value, [ASSET_ID]: 1 }
         }
     ],
-    mint: {
-        action: [{ type: "mint", quantity: 1, asset: ASSET_ID }],
-        script: [mintScript]
-    },
-    // mint: [{ action: "mint", value: 1, token: ASSET_ID }],
+    // mint: {
+    //     actions: [{ type: "mint", quantity: 1, asset: ASSET_ID }],
+    //     script: [mintScript]
+    // },
+    mint: [{ action: "mint", quantity: 1, asset: ASSET_ID }],
     metadata,
     witnessCount: 2
 }
